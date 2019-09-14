d7c96e47aad2fd327eb4ef744346821a3a57b9bc
Mark Manning <mark4th@gmail.com>
2019-09-14 10:45:52 -0500

    removed as most of it was brought into build.shs

--- build-das.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -xueo pipefail
-
-# CC=clang++ CFLAGS="-pedantic -Wall -Werror -std=c++17 -O2 -xc++ -c" make -f Makefile.das u
-CC=clang CFLAGS="-pedantic -Wall -Werror -std=c18 -O2 -xc -c" make -f Makefile.das u

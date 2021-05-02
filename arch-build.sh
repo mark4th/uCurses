9dc1034edd5cc4db31ac3610652b1796e7789319
Dwight Schauer <dwight.schauer@teegra.net>
2021-05-02 09:43:56 -0500

    Reduced size of meson build, as meson provides a lot of defaults we use

--- arch-build.sh
+++ arch-build.sh
@@ -2,21 +2,23 @@
 
 set -euo pipefail
 
-export CC=clang
-export CC_LD=lld
+which clang && CC=${CC:-clang}
+which lld && CC_LD=${CC_LLD:-lld}
+
+export CC CC_LD
 
 PROJECT_ROOT=$(pwd)
 
 ARCH_MESON="${PROJECT_ROOT}/arch.meson"
 ARCH_BUILD="${ARCH_MESON}/arch.build"
-MESON_BUILD='packaging/linux/distro/arch/meson.build'
+BUILD_CONFIG='packaging/linux/distro/arch'
+MESON_OPT=''
+NINJA_OPT='--verbose'
+
+export PROJECT_ROOT ARCH_MESON
 
 rm -rf ${ARCH_MESON}
-mkdir -p ${ARCH_MESON}
-cp -v ${MESON_BUILD} ${ARCH_MESON}
-ln -sv ${PROJECT_ROOT}/src ${ARCH_MESON}/src
-ln -sv ${PROJECT_ROOT}/h ${ARCH_MESON}/h
+cp -av ${BUILD_CONFIG}/ ${ARCH_MESON}/
 
-cd ${ARCH_MESON} && meson setup ${ARCH_BUILD}
-
-cd ${ARCH_BUILD} && ninja
+cd ${ARCH_MESON} && meson setup ${ARCH_BUILD} ${MESON_OPT}
+cd ${ARCH_BUILD} && ninja ${NINJA_OPT}

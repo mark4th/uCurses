dab9c9d18158719b652ca3b504cac4fe508af7cc
Mark I Manning IV <mark4th@gmail.com>
2021-07-09 17:35:41 -0400

    minor cleanup of existing tests
    some changes were made to the framework to simplify and clean up
    assertions and logging.  changed the existing tests to leverage these changes

--- src/TEST/ut_parse.c
+++ src/TEST/ut_parse.c
@@ -1,4 +1,8 @@
+// parse.c -- unit tests
+// -----------------------------------------------------------------------
+
 #include <libchordtest/test.h>
+#include "h/assert.h"
 #include "src/parse.c"
 
 // ------------------------------------------------------------------------
@@ -19,12 +23,8 @@ void our(check_c_emit)(char sent)
     uint16_t prev_num_esc = num_esc;
     c_emit(sent);
     char got = esc_buff[prev_num_esc];
-
-    log_debug(("sent = %02x, got = %02x, num_esc = %d, prev_num_esc = %d",
-        sent, got, num_esc, prev_num_esc));
-
-    ut_assert(num_esc == prev_num_esc + 1);
-    ut_assert(got == sent);
+    ut_assert_op_int16x(num_esc, ==, prev_num_esc + 1);
+    ut_assert_op_int16x(got, ==, sent);
 }
 
 TEST("c_emit", "check for add to esc_buff")
@@ -38,15 +38,12 @@ TEST("c_emit", "check for add to esc_buff")
 
 void our(check_fs_push)(int64_t sent)
 {
-    uint16_t prev_fsp = fsp;
+    int8_t prev_fsp = fsp;
     fs_push(sent);
     int64_t got = fstack[prev_fsp];
 
-    log_debug(("sent = %" PRIx64 ", got = %" PRIx64 ", fsp = %d prev_fsp = %d",
-        sent, got, fsp, prev_fsp));
-
-    ut_assert(fsp == prev_fsp + 1);
-    ut_assert(got == sent);
+    ut_assert_op_int8(fsp, ==, prev_fsp + 1);
+    ut_assert_op_int64x(got, ==, sent);
 }
 
 TEST("fs_push", "check format string statck push")
@@ -62,14 +59,11 @@ void our(check_fs_pop)(int64_t sent)
 {
     fs_push(sent);
 
-    uint16_t prev_fsp = fsp;
+    int8_t prev_fsp = fsp;
     int64_t got = fs_pop();
 
-    log_debug(("sent = %" PRIx64 ", got = %" PRIx64 ", fsp = %d prev_fsp = %d",
-        sent, got, fsp, prev_fsp));
-
-    ut_assert(fsp == prev_fsp - 1);
-    ut_assert(got == sent);
+    ut_assert_op_int8(fsp, ==, prev_fsp - 1);
+    ut_assert_op_int64x(got, ==, sent);
 }
 
 TEST("fs_push", "check format string statck pop")
@@ -83,16 +77,13 @@ TEST("fs_push", "check format string statck pop")
 
 void our(check_percent)(void)
 {
-    uint16_t prev_num_esc = num_esc;
+    int8_t prev_num_esc = num_esc;
     _percent();
     char sent = '%';
     char got = esc_buff[prev_num_esc];
 
-    log_debug(("sent = %02x, got = %02x, num_esc = %d, prev_num_esc = %d",
-        sent, got, num_esc, prev_num_esc));
-
-    ut_assert(num_esc == prev_num_esc + 1);
-    ut_assert(got == sent);
+    ut_assert_op_int16x(num_esc, ==, prev_num_esc + 1);
+    ut_assert_op_int8(got, ==, sent);
 }
 
 TEST("check_percent", "verify addition of '%' char to escape buffer")
@@ -112,10 +103,7 @@ void our(check_and(int64_t n1, int64_t n2))
     _and();
     rv = fs_pop();
 
-    log_debug(("sent = %" PRIx64 ":%" PRIx64 " expected = %" PRIx64 " got = %" PRIx64,
-        n1, n2, rv, n1 & n2));
-
-    ut_assert(rv == (n1 & n2));
+    ut_assert_op_int64x(rv, ==, (n1 & n2));
 }
 
 TEST("_and", "check format string 'and' operator")
@@ -136,10 +124,7 @@ void our(check_and_logical(int64_t n1, int64_t n2))
     _andl();
     rv = fs_pop();
 
-    log_debug(("sent = %" PRIx64 ":%" PRIx64 " expected = %" PRIx64 " got = %d",
-        n1, n2, rv, n1 && n2));
-
-    ut_assert(rv == (n1 && n2));
+    ut_assert_op_int64x(rv, ==, (n1 && n2));
 }
 
 TEST("_andl", "check format string logical 'and' operator")
@@ -160,10 +145,7 @@ void our(check_or(int64_t n1, int64_t n2))
     _or();
     rv = fs_pop();
 
-    log_debug(("sent = %" PRIx64 ":%" PRIx64 " expected = %" PRIx64 " got = %" PRIx64,
-        n1, n2, rv, (n1 | n2)));
-
-    ut_assert(rv == (n1 | n2));
+    ut_assert_op_int64x(rv, ==, (n1 | n2));
 }
 
 TEST("_or", "check format string 'or' operator")
@@ -184,15 +166,10 @@ void our(check_or_logical(int64_t n1, int64_t n2))
     _orl();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64 ":%" PRIx64
-        " expected = %" PRIx64 " got = %d",
-        n1, n2, rv, (n1 || n2)));
-
-    ut_assert(rv == (n1 || n2));
+    ut_assert_op_int64x(rv, ==, (n1 || n2));
 }
 
-TEST("_andl", "check format string logical 'or' operator")
+TEST("_orl", "check format string logical 'or' operator")
 {
     src_parse_init();
     our(check_or_logical(0x55, 0xff));
@@ -209,12 +186,7 @@ void our(check_not(int64_t n1))
     _tilde();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64
-        " expected = %" PRIx64 " got = %" PRIx64,
-        n1, rv, ~n1));
-
-    ut_assert(rv == ~n1);
+    ut_assert_op_int64x(rv, ==, ~n1);
 }
 
 TEST("_tilde", "check format string 'not' operator")
@@ -234,12 +206,7 @@ void our(check_not_logical(int64_t n1))
     _bang();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64
-        " expected = %" PRIx64 " got = %d",
-        n1, rv, !n1));
-
-    ut_assert(rv == !n1);
+    ut_assert_op_int64x(rv, ==, !n1);
 }
 
 TEST("_bang", "check format string logical 'not' operator")
@@ -260,12 +227,7 @@ void our(check_xor(int64_t n1, int64_t n2))
     _caret();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64 ":%" PRIx64
-        " expected = %" PRIx64 " got = %" PRIx64,
-        n1, n2, rv, (n1 ^ n2)));
-
-    ut_assert(rv == (n1 ^ n2));
+    ut_assert_op_int64x(rv, ==, (n1 ^ n2));
 }
 
 TEST("_caret", "check format string 'xor' operator")
@@ -286,12 +248,7 @@ void our(check_plus(int64_t n1, int64_t n2))
     _plus();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64 ":%" PRIx64
-        " expected = %" PRIx64 " got = %" PRIx64,
-        n1, n2, rv, (n1 + n2)));
-
-    ut_assert(rv == (n1 + n2));
+    ut_assert_op_int64x(rv, ==, (n1 + n2));
 }
 
 TEST("_plus", "check format string '+' operator")
@@ -312,12 +269,7 @@ void our(check_minus(int64_t n1, int64_t n2))
     _minus();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64 ":%" PRIx64
-        " expected = %" PRIx64 " got = %" PRIx64,
-        n1, n2, rv, (n1 - n2)));
-
-    ut_assert(rv == (n1 - n2));
+    ut_assert_op_int64x(rv, ==, (n1 - n2));
 }
 
 TEST("_minus", "check format string '-' operator")
@@ -338,12 +290,7 @@ void our(check_star(int64_t n1, int64_t n2))
     _star();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64 ":%" PRIx64
-        " expected = %" PRIx64 " got = %" PRIx64,
-        n1, n2, rv, (n1 * n2)));
-
-    ut_assert(rv == (n1 * n2));
+    ut_assert_op_int64x(rv, ==, (n1 * n2));
 }
 
 TEST("_star", "check format string '*' operator")
@@ -364,12 +311,7 @@ void our(check_slash(int64_t n1, int64_t n2))
     _slash();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64 ":%" PRIx64
-        " expected = %" PRIx64 " got = %" PRIx64,
-        n1, n2, rv, (n1 / n2)));
-
-    ut_assert(rv == (n1 / n2));
+    ut_assert_op_int64x(rv, ==, (n1 / n2));
 }
 
 TEST("_slash", "check format string '/' operator")
@@ -390,12 +332,7 @@ void our(check_mod(int64_t n1, int64_t n2))
     _mod();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64 ":%" PRIx64
-        " expected = %" PRIx64 " got = %" PRIx64,
-        n1, n2, rv, (n1 % n2)));
-
-    ut_assert(rv == (n1 % n2));
+    ut_assert_op_int64x(rv, ==, (n1 % n2));
 }
 
 TEST("_mod", "check format string 'mod' operator")
@@ -416,12 +353,7 @@ void our(check_equals(int64_t n1, int64_t n2))
     _equals();
     rv = fs_pop();
 
-    log_debug((
-        "sent = %"  PRIx64 ":%" PRIx64
-        " expected = %" PRIx64 " got = %d",
-        n1, n2, rv, (n1 == n2)));
-
-    ut_assert(rv == (n1 == n2));
+    ut_assert_op_int64x(rv, ==, (n1 == n2));
 }
 
 TEST("_mod", "check format string '=' operator")
@@ -431,10 +363,69 @@ TEST("_mod", "check format string '=' operator")
     our(check_mod(10, 2));
 }
 
+// -----------------------------------------------------------------------
+
+void our(check_greater(int64_t n1, int64_t n2))
+{
+    int64_t rv;
+
+    fs_push(n1);
+    fs_push(n2);
+    _greater();
+    rv = fs_pop();
+
+    ut_assert_op_int64x(rv, ==, (n1 > n2));
+}
+
+TEST("_greater", "check format string '>' operator")
+{
+    src_parse_init();
+    our(check_greater(0x22, 0x55));
+    our(check_greater(10, 2));
+}
 
 // -----------------------------------------------------------------------
+
+void our(check_less(int64_t n1, int64_t n2))
+{
+    int64_t rv;
+
+    fs_push(n1);
+    fs_push(n2);
+    _less();
+    rv = fs_pop();
+
+    ut_assert_op_int64x(rv, ==, (n1 < n2));
+}
+
+TEST("_less", "check format string '>' operator")
+{
+    src_parse_init();
+    our(check_less(0x22, 0x55));
+    our(check_less(10, 2));
+}
+
 // -----------------------------------------------------------------------
-// -----------------------------------------------------------------------
+
+void our(check_tick(char c))
+{
+    f_str = &c;
+    int8_t prev_num_esc = num_esc;
+
+    _tick();
+    char got = esc_buff[prev_num_esc];
+
+    ut_assert_op_int64x(got, ==, c);
+}
+
+TEST("_tick", "check format string literal insertion")
+{
+    src_parse_init();
+    our(check_tick('x'));
+    our(check_tick('z'));
+}
+
+
 // -----------------------------------------------------------------------
 // -----------------------------------------------------------------------
 // -----------------------------------------------------------------------
